87987365fb0d5aaea300565ab62ac0bd2205a3cd
JoceCasteletti <jocelyncasteletti@gmail.com>
2019-04-04 12:05:06 -0300

    se agrega cifrar para minúsculas,números y espacio

--- src/cipher.js
+++ src/cipher.js
@@ -8,6 +8,18 @@ window.cipher = {
       if (asciiNumber >= 65 && asciiNumber <=90) {
         asciiNumber = ((asciiNumber - 65 + offset) % 26) + 65;
       }
+      
+      if (asciiNumber >= 97 && asciiNumber <=122){
+        asciiNumber = ((asciiNumber - 97 + offset) % 26) + 97;
+      }
+
+      if (asciiNumber >= 48 && asciiNumber <=57){
+        asciiNumber = ((asciiNumber - 48 + offset) % 10) + 48;
+      }
+
+      if (asciiNumber == 32) {
+        asciiNumber = ((asciiNumber - 32 + offset) % 1) + 32;
+      }
 
       result += String.fromCharCode(asciiNumber);
     }
