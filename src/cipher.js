eecd6cb78c9aeb94f6b2447fd64bbc7f8310c8f8
JoceCasteletti <jocelyncasteletti@gmail.com>
2019-04-04 11:00:19 -0300

    se agrega html dinámico y cifrado con letras mayúsculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,18 @@
 window.cipher = {
-  encode: (ofsset,text) => {
-    let encode = '';
-    /* Acá va tu código */
-    for(let i=0; i=<text.length,i++){
-  }
+  encode: (offset, message) => {
+    let result = '';
+   
+    for (let i=0; i < message.length; i++) {
+      let asciiNumber = message.charCodeAt(i);
+
+      if (asciiNumber >= 65 && asciiNumber <=90) {
+        asciiNumber = ((asciiNumber - 65 + offset) % 26) + 65;
+      }
+
+      result += String.fromCharCode(asciiNumber);
+    }
     return result;
   },
-
-
 
   decode: (message,ofset) => {
     /* Acá va tu código */
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-const encode = document.getElementById('')
-const decode = document.getElementById('')
+const encode = document.getElementById('encode');
+const decode = document.getElementById('decode');
+const ejemplo = document.getElementById('ejemplo');
+const start = document.getElementById('start');
+const container = document.getElementById('container');
+
+// https://developer.mozilla.org/en-US/docs/Web/Events/keypress
+ejemplo.addEventListener('keypress', (event) => {
+    if (!(event.keyCode >= 65 && event.keyCode <=90)) {
+        event.preventDefault();
+    }
+})
+
+
+start.addEventListener('click', () => {
+    // Aquí va lógica de validación de comprobación de nombre ante de continuar
+
+   if (document.getElementById('name').value.length == 0) {
+       alert('ingrese nombre')
+   } else {
+     ex1.style.display = 'block';
+   }
+
+
+
+
+})
+
+
+
 
