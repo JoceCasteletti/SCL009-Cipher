d204ca4fb8be57577ed66103d176554215fc3de0
JoceCasteletti <jocelyncasteletti@gmail.com>
2019-04-08 08:57:14 -0400

    se mejora estructural html y se agregan elementos del Dom

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,21 @@
 window.cipher = {
   encode: (offset, message) => {
-    let result_encode = '';
-   
+    let resultEncode = '';
+
     //Recorrer el mensaje , almacenarlo y convertirlo a código Ascii
-    for (let i=0; i < message.length; i++) {
+    for (let i = 0; i < message.length; i++) {
       let asciiNumber = message.charCodeAt(i);//pasar la letra a código Ascii
-     
+
       //Mayúsculas
-      if (asciiNumber >= 65 && asciiNumber <=90) {
+      if (asciiNumber >= 65 && asciiNumber <= 90) {
         asciiNumber = ((asciiNumber - 65 + parseInt(offset)) % 26) + 65;
       }
       //Minúsculas
-      else if (asciiNumber >= 97 && asciiNumber <=122){
+      else if (asciiNumber >= 97 && asciiNumber <= 122) {
         asciiNumber = ((asciiNumber - 97 + parseInt(offset)) % 26) + 97;
       }
       //Números
-      else if (asciiNumber >= 48 && asciiNumber <=57){
+      else if (asciiNumber >= 48 && asciiNumber <= 57) {
         asciiNumber = ((asciiNumber - 48 + parseInt(offset)) % 10) + 48;
       }
       //Espacio
@@ -23,54 +23,50 @@ window.cipher = {
         asciiNumber = ((asciiNumber - 32 + parseInt(offset)) % 1) + 32;
       }
       //Carácteres
-      else if (asciiNumber >= 33 && asciiNumber <=47){
+      else if (asciiNumber >= 33 && asciiNumber <= 47) {
         asciiNumber = ((asciiNumber - 33 + parseInt(offset)) % 15) + 47;
       }
       //Otros carácteres
-      else if (asciiNumber >= 58 && asciiNumber <=64){
+      else if (asciiNumber >= 58 && asciiNumber <= 64) {
         asciiNumber = ((asciiNumber - 58 + parseInt(offset)) % 7) + 64;
       }
-      
+
       resultEncode += String.fromCharCode(asciiNumber);
     }
     return resultEncode;
   },
 
-  decode: (message,oFfset) => {
-      let result_decode ='';
-      let newOffset = 0;
-  
-      
-    
-      for (let i=0; i < message.length; i++) {
-        let asciiNumber = message.charCodeAt(i);
-      
-          // Mayúsculas
-          if (asciiNumber >= 65 && asciiNumber <=90) {
-            newOffset =  26 - offset;
-            asciiNumber = ((asciiNumber - 65 + parseInt(newOffset)) % 26) + 65;
-          }
-          //Minúsculas
-          else if (asciiNumber >= 97 && asciiNumber <=122) {
-            newOffset =  26 - offset;
-            asciiNumber = ((asciiNumber - 97 + parseInt(newOffset)) % 26) + 97;
-          }
-          //Números
-          else if (asciiNumber >= 48 && asciiNumber <=57){
-            newOffset =  10 - offset;
-            asciiNumber = ((asciiNumber - 48 + parseInt(newOffset)) % 10) + 48;
-          }
-          //Espacio
-          else if (asciiNumber == 32) {
-            asciiNumber = ((asciiNumber - 32 + parseInt(newOffset)) % 1) + 32;
-          }
-  
-          resultDecode = resultDecode + String.fromCharCode(asciiNumber);
-        } 
-        
-        return resultDecode;
-  
+  decode: (offset, message) => {
+    let resultDecode = '';
+    let newOffset = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      let asciiNumber = message.charCodeAt(i);
+
+      // Mayúsculas
+      if (asciiNumber >= 65 && asciiNumber <= 90) {
+        newOffset = 26 - offset;
+        asciiNumber = ((asciiNumber - 65 + parseInt(newOffset)) % 26) + 65;
       }
+      //Minúsculas
+      else if (asciiNumber >= 97 && asciiNumber <= 122) {
+        newOffset = 26 - offset;
+        asciiNumber = ((asciiNumber - 97 + parseInt(newOffset)) % 26) + 97;
+      }
+      //Números
+      else if (asciiNumber >= 48 && asciiNumber <= 57) {
+        newOffset = 10 - offset;
+        asciiNumber = ((asciiNumber - 48 + parseInt(newOffset)) % 10) + 48;
+      }
+      //Espacio
+      else if (asciiNumber == 32) {
+        asciiNumber = ((asciiNumber - 32 + parseInt(newOffset)) % 1) + 32;
+      }
+
+      resultDecode = resultDecode + String.fromCharCode(asciiNumber);
     }
+
+    return resultDecode;
+
   }
 };
--- src/index.js
+++ src/index.js
@@ -4,12 +4,34 @@ const message_text = document.getElementById('message_text');
 const start = document.getElementById('start');
 const container = document.getElementById('container');
 
+
+const encodeMessage = document.getElementById('encode_message');
+const encodeOffset = document.getElementById('encode_offset');
+const resultContainer = document.getElementById('result_container');
+const result = document.getElementById('result');
+
 // https://developer.mozilla.org/en-US/docs/Web/Events/keypress
-ejemplo.addEventListener('keypress', (event) => {
+encodeMessage.addEventListener('keypress', (event) => {
     if (!(event.keyCode >= 65 && event.keyCode <=90)) {
         event.preventDefault();
     }
 })
+
+encode.addEventListener('click', () => {
+    if (encodeMessage.value.length == 0) {
+        alert('Ingrese algun mensaje por favor');
+        return;
+    } else if (!Number.isInteger(encodeOffset.value) && encodeOffset.value == 0) {
+        // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Number/isInteger
+        alert('Ingrese número válido por favor');
+        return;
+    }
+
+    // Lista de las clases del elemento del DOM
+    resultContainer.classList.remove('hidden');
+
+    result.value = window.cipher.encode(encodeOffset.value, encodeMessage.value);
+});
 
 
 start.addEventListener('click', () => {
